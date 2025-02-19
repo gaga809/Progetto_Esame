a111fb01ce963f0cd856fdebd68c12aef1819318
Andrea Vallauri <125751096+AndreaBoassoVallauri@users.noreply.github.com>
2025-02-19 07:33:44 +0000

    Rimozione package json dalla root crezione custom-header component

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ScrollComponent } from './components/scroll/scroll.component';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ScrollComponent],
+  imports: [ScrollComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
