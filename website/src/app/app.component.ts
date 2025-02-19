13452eecb81ca26877762ba782153c3af19da04f
Andrea Vallauri <125751096+AndreaBoassoVallauri@users.noreply.github.com>
2025-02-19 08:34:16 +0000

    Creazione impaginazione video

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ScrollComponent } from './components/scroll/scroll.component';
 import { HeaderComponent } from "./components/header/header.component";
+import { VideosComponent } from "./components/videos/videos.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ScrollComponent, HeaderComponent],
+  imports: [ScrollComponent, HeaderComponent, VideosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
