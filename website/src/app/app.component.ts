0ea92d48eb00bc23dea50c78726df8cead0543d3
Andrea Vallauri <125751096+AndreaBoassoVallauri@users.noreply.github.com>
2025-02-19 09:26:03 +0000

    Creazione notizie component

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ScrollComponent } from './components/scroll/scroll.component';
 import { HeaderComponent } from "./components/header/header.component";
 import { VideosComponent } from "./components/videos/videos.component";
+import { NewsComponent } from "./components/news/news.component";
 
 @Component({
   selector: 'app-root',
-  imports: [ScrollComponent, HeaderComponent, VideosComponent],
+  imports: [ScrollComponent, HeaderComponent, VideosComponent, NewsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
