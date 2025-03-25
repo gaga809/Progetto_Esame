89c3e3175a44f8879b9922b9243fdad9cf5352d0
Gaga Postation <gabrielferrero7306@gmail.com>
2025-03-25 22:50:14 +0100

    AGGIUNTA NON FUNZIONANTE: Danno e vita dei giocatori e dei mob.

--- unity_proj/Assets/Scripts/Mob/MobModel.cs
+++ unity_proj/Assets/Scripts/Mob/MobModel.cs
@@ -1,22 +1,30 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class MobModel : MonoBehaviour
 {
     [Header("Player Settings")]
-    public Transform player;
+    public GameObject player;
 
     [Header("Mob Settings")]
+    public int health = 5;
+    public int maxHealth = 5;
     public float speed = 3.5f;
     public float acceleration = 8f;
     public float stoppingDistance = 2f;
+    public int attackDamage = 1;
+    public float attackRate = 1f;
 
     private NavMeshAgent agent;
-    private Vector3 lastPosition = Vector3.zero;   
+    private Vector3 lastPosition = Vector3.zero;
+    private Transform trsPly;
+    private bool canAttack = true;
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
+        player = GameObject.FindGameObjectWithTag("Player");
+        trsPly = player.transform;
 
         agent = GetComponent<NavMeshAgent>();
         agent.speed = speed;
@@ -33,23 +41,43 @@ public class MobModel : MonoBehaviour
     {
         if (player != null)
         {
-            float distance = Vector3.Distance(transform.position, player.position);
+            float distance = Vector3.Distance(transform.position, trsPly.position);
             Debug.Log(distance);
             if (distance > stoppingDistance)
             {
-                if (lastPosition != player.position)
+                if (lastPosition != trsPly.position)
                 {
-                    lastPosition = player.position;
-                    agent.SetDestination(player.position);
+                    lastPosition = trsPly.position;
+                    agent.SetDestination(trsPly.position);
                 }
             }
             else
             {
-                //agent.ResetPath();
                 Debug.Log("Il nemico è vicino al giocatore.");
+                if (canAttack)
+                {
+                    StartCoroutine(CooldownAttack());
+                    player.GetComponent<PlayerModel>().Hurt(attackDamage);
+                }
             }
 
             
+        }
+    }
+
+    IEnumerator CooldownAttack()
+    {
+        canAttack = false;
+        yield return new WaitForSeconds(attackRate);
+        canAttack = true;
+    }
+
+    public void Hurt(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
         }
     }
 }
--- unity_proj/Assets/Scripts/Players/PlayerModel.cs
+++ unity_proj/Assets/Scripts/Players/PlayerModel.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerModel : MonoBehaviour
 {
     [Header("Player Settings")]
+    public int health = 10;
+    public int maxHealth = 10;
     public float speed = 5f;
     public float deceleration = 5f;
     public float minSpeed = 0.1f;
@@ -54,5 +56,32 @@ public class PlayerModel : MonoBehaviour
         playerControls.actions["Jump"].performed -= ctx => playerController.OnJump();
         playerControls.actions["Attack"].performed -= ctx => playerController.OnAttack();
         playerControls.actions["Interact"].performed -= ctx => playerController.OnInteract();
+    }
+
+    public void Hurt(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Die();
+        }
+
+        Debug.Log("Il Giocatore ha subito " + damage + " danni. Saldo: " + health);
+    }
+
+    public void Heal(int amount)
+    {
+        health += amount;
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+
+        Debug.Log("Il Giocatore ha recuperato " + amount + " punti vita. Saldo: " + health);
+    }
+
+    public void Die()
+    {
+        Debug.Log("Il Giocatore è schiattato. Porcacci");
     }
 }
