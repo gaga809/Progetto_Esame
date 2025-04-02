17520e00ddb13a5a837d3c09bac08fee3a6011eb
Andrea Vallauri <125751096+AndreaBoassoVallauri@users.noreply.github.com>
2025-04-02 17:07:46 +0000

    Implementazione traduttore lingue

--- website/src/app/components/header/header.component.ts
+++ website/src/app/components/header/header.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'custom-header',
-  imports: [],
+  standalone: true,
+  imports: [TranslateModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private translateService: TranslateService) {}
 
+  onLanguageChange(event: Event) {
+    const selectedLanguage = (event.target as HTMLSelectElement).value;
+    this.translateService.use(selectedLanguage);
+  }
 }
