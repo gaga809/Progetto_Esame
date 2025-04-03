e8c04225c3b4c376a4dc3e6449887dcf2d32006a
Andrea Vallauri <125751096+AndreaBoassoVallauri@users.noreply.github.com>
2025-04-03 21:30:01 +0000

    implementazione grafica select nazioni e bounce di div

--- website/src/app/components/header/header.component.ts
+++ website/src/app/components/header/header.component.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'custom-header',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, UpperCasePipe],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  selectedLanguage: string = 'English'; 
+  selectedLanguageFlag: string = 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Flag_of_the_United_States.svg'; 
   constructor(private translateService: TranslateService) {}
+  dropdownVisible: boolean = false; 
 
-  onLanguageChange(event: Event) {
-    const selectedLanguage = (event.target as HTMLSelectElement).value;
-    this.translateService.use(selectedLanguage);
+  toggleDropdown() {
+    this.dropdownVisible = !this.dropdownVisible;
+  }
+
+  onLanguageChange(language: string, flagUrl: string, nationality: string) {
+    this.selectedLanguage = nationality;
+    this.selectedLanguageFlag = flagUrl;
+
+    this.dropdownVisible = false;
+
+    this.translateService.use(language);
   }
 }
