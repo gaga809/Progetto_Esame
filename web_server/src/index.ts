021d1eceecc7c7d080849020d8283a9d8d98918e
Gaga Postation <gabrielferrero7306@gmail.com>
2025-04-12 22:12:23 +0200

    AGGIUNTA: Supporto Swagger e file statici

--- web_server/src/index.ts
+++ web_server/src/index.ts
@@ -1,5 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+// Local Imports
 import apiV1Router from "./routes/apiv1/apiv1";
 import logger from "./utils/logger";
 
@@ -8,6 +12,21 @@ logger.setLevel("INDEX");
 
 // Load environment variables from .env file
 dotenv.config();
+
+// Swagger setup
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "API Documentation",
+            version: "0.0.1",
+            description: "The API documentation for the project",
+        },
+    },
+    apis: ["./src/routes/apiv1/*.ts"],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 // Environment variables
 const PORT = process.env.PORT || 3000;
@@ -18,11 +37,14 @@ function setup() {
 
     app.use(express.json());
 
-    app.use("/api/v1", apiV1Router);
+    // public folder for static files
+    app.use(express.static("public"));
 
-    app.get("/", (req, res) => {
-        res.send("Hello World!");
-    });
+    // Swagger UI setup
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+    // Show Main Route
+    app.use("/api/v1", apiV1Router);
 
     app.listen(PORT, () => {
         logger.info(`Server is running on http://localhost:${PORT}`);
--- web_server/src/routes/apiv1/apiv1.ts
+++ web_server/src/routes/apiv1/apiv1.ts
@@ -1,6 +1,20 @@
 // src/routes/apiv1/apiv1.ts
-import { Router } from 'express';
+import { Router } from "express";
 
 const router = Router();
+
+/**
+ * @swagger
+ * /api/v1:
+ *   get:
+ *     summary: Get the API version
+ *     description: Returns a message indicating that the API is working.
+ *     responses:
+ *       200:
+ *         description: A message indicating that the API is working.
+ */
+router.get("/", (req, res) => {
+    res.send("API v1 is working!");
+});
 
 export default router;
