ed8ac0dc974f57d58b8de172c573ed66562c7fa1
Gaga Mobil <gabrielferrero7306@gmail.com>
2025-04-16 13:09:20 +0200

    AGGIUNTA: Server https e redirect da http a https. Creazione di certificati self-signed

--- web_server/src/index.ts
+++ web_server/src/index.ts
@@ -2,6 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import https from "https";
+import http from "http";
+import { execSync } from "child_process";
 
 // Local Imports
 import apiV1Router from "./routes/apiv1/apiv1";
@@ -20,67 +24,126 @@ const db = SvlimeDatabase.getInstance();
 
 // Swagger setup
 const swaggerOptions = {
-    definition: {
-        openapi: "3.0.0",
-        info: {
-            title: "API Documentation",
-            version: "0.0.1",
-            description:
-                "The documentation for the Svlime API. Endpoints are listed below.",
-        },
-        components: {
-            securitySchemes: {
-                BearerAuth: {
-                    type: "http",
-                    scheme: "bearer",
-                    bearerFormat: "JWT",
-                    description:
-                        "Enter your bearer token in the format **Bearer {token}**",
-                },
-            },
-        },
-        security: [
-            {
-                BearerAuth: [],
-            },
-        ],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Documentation",
+      version: "0.0.1",
+      description:
+        "The documentation for the Svlime API. Endpoints are listed below.",
     },
-    apis: ["./src/routes/apiv1/*.ts"],
+    components: {
+      securitySchemes: {
+        BearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+          description:
+            "Enter your bearer token in the format **Bearer {token}**",
+        },
+      },
+    },
+    security: [
+      {
+        BearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./src/routes/apiv1/*.ts"],
 };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 
 // Environment variables
-const PORT = process.env.PORT || 3000;
+const HTTPPORT = process.env.HTTPPORT ? parseInt(process.env.HTTPPORT) : 80;
+const HTTPSPORT = process.env.HTTPSPORT ? parseInt(process.env.HTTPSPORT) : 4433;
+const CERT_FILE = process.env.CERT_FILE || "./cert.pem";
+const KEY_FILE = process.env.KEY_FILE || "./key.pem";
+
+// Check for certificate (HTTPS)
+
+function hasOpenSSL() {
+  try {
+    execSync("openssl version", { stdio: "ignore" });
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+if (!fs.existsSync(CERT_FILE) || !fs.existsSync(KEY_FILE)) {
+  logger.warn("Couldn't find a certificate for HTTPS.");
+  if (hasOpenSSL()) {
+    logger.info(
+      "OpenSSL is available. Generating a self-signed certificate..."
+    );
+    try {
+      execSync(
+        `openssl req -x509 -newkey rsa:2048 -nodes -keyout ${KEY_FILE} -out ${CERT_FILE} -days 365 -subj "/CN=localhost"`
+      );
+      logger.info("Certificate generated successfully.");
+    } catch (err: any) {
+      logger.error("Error generating certificate: " + err.message);
+      process.exit(1);
+    }
+  } else {
+    logger.error(
+      `OpenSSL is not available. Cannot generate a self-signed certificate. Missing ${CERT_FILE} and ${KEY_FILE}. Create them manually.`
+    );
+    process.exit(1);
+  }
+}
 
 // Express Server
 async function setup() {
-    await db.connect();
+  await db.connect();
 
-    if(db.getConnection() != null){
-        logger.info("Starting server...");
+  if (db.getConnection() != null) {
+    logger.info("Starting server...");
 
-        const app = express();
+    const app = express();
 
-        app.use(express.json());
-    
-        // public folder for static files
-        app.use(express.static("public"));
-    
-        // Swagger UI setup
-        app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-    
-        // Show Main Route
-        app.use("/api/v1", apiV1Router);
-    
-        app.listen(PORT, () => {
-            logger.info(`Server is running on http://localhost:${PORT}`);
+    app.use(express.json());
+
+    // public folder for static files
+    app.use(express.static("public"));
+
+    // Swagger UI setup
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+    // Show Main Route
+    app.use("/api/v1", apiV1Router);
+
+    https
+      .createServer(
+        {
+          key: fs.readFileSync(KEY_FILE),
+          cert: fs.readFileSync(CERT_FILE),
+        },
+        app
+      )
+      .listen(HTTPSPORT, () => {
+        logger.info(
+          `Server HTTPS is running on https://localhost:${HTTPSPORT}`
+        );
+      });
+
+    http
+      .createServer((req, res) => {
+        res.writeHead(HTTPPORT, {
+          Location:
+            "https://" + req.headers.host?.replace(/:\d+/, ":"+HTTPSPORT) + req.url,
         });
-    }else{
-        return;
-    }    
+        res.end();
+      })
+      .listen(301, () => {
+        logger.info(`The HTTP is running on http://localhost:${HTTPPORT}. Redirecting to HTTPS server...`);
+      });
+  } else {
+    return;
+  }
 }
 
 if (require.main === module) {
-    setup();
+  setup();
 }
